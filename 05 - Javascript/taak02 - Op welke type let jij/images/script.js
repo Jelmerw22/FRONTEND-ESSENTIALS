890fefcc7bb48515d9b8f5ff840c90d7f6d4b63e
= <=>
2020-11-10 11:08:57 +0100

    java script

---
+++ 05 - Javascript/taak02 - Op welke type let jij/images/script.js
@@ -0,0 +1,8 @@
+let mijnVoornaam = "Jelmer.";
+let mijnAchternaam = 'Wiersma.';
+let beroep = "Student.";
+let school = 'ROC van Amsterdam.';
+let leeftijd = '17';
+let ikBenStemrechtig = false;
+let ikMagLegaalAlcoholKopent = false;
+let ikMagBrommerRijden = false;
