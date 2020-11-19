b7a12d04b7055978e881a49b4a0e6b760c21b6c9
Jelmer Wiersma <jelmer.wiersma@student.rocva.nl>
2020-11-19 13:05:40 +0100

    proberen het te laten werken

--- opdracht/eindopdracht/script.js
+++ opdracht/eindopdracht/script.js
@@ -1,17 +1,16 @@
 let bezoeker = {
     Voornaam : prompt("Wat is uw voornaam?"),
-    achternaam : prompt("Wat is uw achternaam?"),
-    leeftijd : prompt("Hoe oud ben u?"),
+    Achternaam : prompt("Wat is uw achternaam?"),
+    Leeftijd : prompt("Hoe oud ben u?"),
     Woonplaats : prompt("Wat is uw woonplaats?"),
-    Email : prompt("Wat is uw e-mailadres?"),
-};
+    Email : prompt("Wat is uw e-mailadres?")
+}
 console.log(bezoeker);
 if(bezoeker.leeftijd==16) {
     document.body.style.backgroundColor="Chocolate" 
 } else {
     document.body.style.backgroundColor="Black"
-};
-
+}
 if(bezoeker.leeftijd==17) {
     document.body.style.backgroundColor="Aqua"
 }
@@ -21,4 +20,3 @@ if(bezoeker.leeftijd==18) {
 if(bezoeker.leeftijd==19) {
     document.body.style.backgroundColor="Chartreuse"
 }
-var x = document.getElementById("tabel");
